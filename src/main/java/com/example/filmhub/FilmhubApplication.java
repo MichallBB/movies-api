e75e9f1c50957e3eae467a6d6b84ec84afdc55a0
MichallBB <98035204+MichallBB@users.noreply.github.com>
2023-11-08 19:58:58 +0100

    new file:   .gitignore 	new file:   .mvn/wrapper/maven-wrapper.jar 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/com/example/filmhub/FilmhubApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/example/filmhub/FilmhubApplicationTests.java

---
+++ src/main/java/com/example/filmhub/FilmhubApplication.java
@@ -0,0 +1,13 @@
+package com.example.filmhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmhubApplication.class, args);
+	}
+
+}
