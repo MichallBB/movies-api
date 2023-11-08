68fe4aa9a4b060cb66d6bc968a129305989deae6
MichallBB <98035204+MichallBB@users.noreply.github.com>
2023-11-08 20:06:17 +0100

    yaml

--- src/main/java/com/example/filmhub/FilmhubApplication.java
+++ src/main/java/com/example/filmhub/FilmhubApplication.java
@@ -11,3 +11,25 @@ public class FilmhubApplication {
 	}
 
 }
+//        movie{
+//            id:
+//            name:
+//            prod_date:
+//            produciont_country:
+//            count_of_people_rated:
+//            sum_of_rating:
+//            description:
+//            actor[
+//                    id:
+//                    real_name:
+//                    name_in_movie:
+//                    ]
+//
+//        }
+//        account{
+//            id:
+//            name:
+//            rated_movies:
+//            want_to_see:
+//            favorites:
+//        }
